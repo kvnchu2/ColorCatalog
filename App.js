66a93a63c33a605302f2e57c4ae0aa6c5cc3351d
Kevin Chu <kvnchu2@gmail.com>
2021-07-18 19:40:55 -0700

    add custom component colorbutton

--- App.js
+++ App.js
@@ -2,17 +2,25 @@ import React, { useState } from 'react';
 import { StyleSheet, View, Text, TouchableHighlight } from 'react-native';
 import icon from "./assets/icon.png";
 
-
+function ColorButton({backgroundColor, onPress}) {
+  return (
+    <TouchableHighlight style={styles.button} onPress={() => onPress(backgroundColor) } underlayColor="orange">
+        <View style={styles.row}>
+          <View style={[styles.sample, {backgroundColor}]}></View>
+          <Text style={styles.buttonText}>{backgroundColor}</Text>
+        </View>
+    </TouchableHighlight>
+  )
+}
 export default function App() {
   const [backgroundColor, setBackgroundColor] = useState("blue");
   return(
     <View style={ [styles.container, {backgroundColor}]}>
-      <TouchableHighlight style={styles.button} onPress={() => setBackgroundColor("yellow") } underlayColor="orange">
-        <View style={styles.row}>
-          <View style={[styles.sample, {backgroundColor: "yellow"}]}></View>
-          <Text style={styles.buttonText}>yellow</Text>
-        </View>
-      </TouchableHighlight>
+      <ColorButton backgroundColor="red" onPress={setBackgroundColor}/>
+      <ColorButton backgroundColor="green" onPress={setBackgroundColor}/>
+      <ColorButton backgroundColor="blue" onPress={setBackgroundColor}/>
+      <ColorButton backgroundColor="yellow" onPress={setBackgroundColor}/>
+      <ColorButton backgroundColor="purple" onPress={setBackgroundColor}/>
     </View>
   )
 }
